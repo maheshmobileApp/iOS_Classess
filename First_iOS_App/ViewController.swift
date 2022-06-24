bcf66e913ab01f24bba216eec05c6728e0da5205
maheshmobileApp <yernagulamahesh@gmail.com>
2022-06-24 09:44:13 +0530

    intial setup

--- First_iOS_App/ViewController.swift
+++ First_iOS_App/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    @IBOutlet weak var pressBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //setPropertiesToLabel()
+    }
+    
+    func setPropertiesToLabel(){
+        self.nameLbl.text = "Hello Sai";
+        self.nameLbl.numberOfLines = 2;
+        self.nameLbl.textColor = UIColor.white;
     }
 
-
+    @IBAction func changeBgColor(_ sender: UIButton) {
+        setPropertiesToLabel();
+        self.view.backgroundColor = UIColor.green;
+    }
+    
+    
 }
 
+/*
+ 
+ View Controll life cycle
+ 
+ 1. label properties
+ 2. button properties
+ 3. set button bg color random
+ 
+ */
