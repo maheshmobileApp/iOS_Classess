aaccdab6623dca41e6d0b03f4aa8c17c57af1ed1
maheshmobileApp <yernagulamahesh@gmail.com>
2022-06-27 09:34:39 +0530

    (Class 3) 1. Explain The TextField Delegate

--- First_iOS_App/ViewController.swift
+++ First_iOS_App/ViewController.swift
@@ -7,15 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var nameLbl: UILabel!
     
-    @IBOutlet weak var pressBtn: UIButton!
+    @IBOutlet weak var inputTextField: UITextField!
+   @IBOutlet weak var pressBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //setPropertiesToLabel()
+        self.inputTextField.delegate = self;
+        self.inputTextField.becomeFirstResponder();
+        
     }
     
     func setPropertiesToLabel(){
@@ -27,8 +33,12 @@ class ViewController: UIViewController {
     @IBAction func changeBgColor(_ sender: UIButton) {
         setPropertiesToLabel();
         self.view.backgroundColor = UIColor.green;
+        self.nameLbl.text = self.inputTextField.text;
     }
-    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
     
 }
 
@@ -38,6 +48,10 @@ class ViewController: UIViewController {
  
  1. label properties
  2. button properties
- 3. set button bg color random
+ 
+ //3. alpha
+ //4 .number
+ //5. 30 max
+ 
  
  */
